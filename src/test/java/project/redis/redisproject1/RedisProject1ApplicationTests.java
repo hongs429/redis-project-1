4192b1a0c99daa0e36cc1a19b797c9e09286ebfd
hongs429 <honshyeon@gmail.com>
2025-01-09 19:22:01 +0900

    first commit

---
+++ src/test/java/project/redis/redisproject1/RedisProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.redis.redisproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
