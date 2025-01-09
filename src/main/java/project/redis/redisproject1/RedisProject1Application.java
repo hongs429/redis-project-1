713e05b2c4f719a3724c6f3b5754f7b62ca978cc
hongs429 <honshyeon@gmail.com>
2025-01-09 19:22:01 +0900

    first commit

---
+++ src/main/java/project/redis/redisproject1/RedisProject1Application.java
@@ -0,0 +1,13 @@
+package project.redis.redisproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisProject1Application.class, args);
+    }
+
+}
