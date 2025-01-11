e40aaf91da7d0cffdf8e73c6b66b0bae1c240fa2
hongs429 <honshyeon@gmail.com>
2025-01-12 04:46:47 +0900

    commit 내용
    - module 구분: presentation, application, infrastructure, domain
    - 각 모듈이 독립적으로 작업가능하도록 SRP 적용
    - Port-Adapter 패턴으로 명확한 의존관계 설정
    - 프로잭트 설명은 루트의 README.md 에 기재
    - 각 모듈의 기능과 설명은 README.md 에 명확하게 기재
    - 급하게 하다보니 커밋을 나누지 못했습니다. 대신, 그만큼 자세하게 README.md에 기재했으니 참고 부탁드립니다

--- module-domain/src/main/java/project/redis/domain/infra/movie/repository/MovieJpaRepository.java
+++
@@ -1,8 +0,0 @@
-//package project.redis.infrastructure.movie.repository;
-//
-//import java.util.UUID;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import project.redis.infrastructure.movie.entity.MovieJpaEntity;
-//
-//public interface MovieJpaRepository extends JpaRepository<MovieJpaEntity, UUID> {
-//}
